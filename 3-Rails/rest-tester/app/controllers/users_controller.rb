84fb5060edbc7dbb99d8a34f65502e2cf18c688e
todd <tmatthews0020@gmail.com>
2015-01-27 15:00:35 -0500

    folder restructure

---
+++ 3-Rails/rest-tester/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+
+  def create
+    @name = "index"
+  end
+  def new
+    @name = "new"
+  end
+  def show
+    @name = "show"
+  end
+  def edit
+    @name = "edit"
+  end
+
+end
